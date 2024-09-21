130edccc569875a25f6c05e4683762128fe51361
vipbaghel <146691079+vipbaghel@users.noreply.github.com>
2024-09-21 17:54:40 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
+import path from 'path';
+
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -13,6 +15,9 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch(err=>{
     console.log(err);
 });
+
+const __dirname = path.resolve();
+
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
@@ -27,6 +32,13 @@ app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/post',postRoutes);
 app.use('/api/comment',commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+});
+
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode ||500;
     const message=err.message || 'Internal Server Error';
